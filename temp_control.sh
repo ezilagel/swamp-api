4d6563989b8f1f90272a456868a17c69869cd3cf
ezilagel <bzaff2012@gmail.com>
2018-06-28 06:47:57 +0000

    Swamp now shuts off when 0 temp difference despite fan status of HVAC

--- temp_control.sh
+++ temp_control.sh
@@ -16,6 +16,7 @@ while true; do
 	set_cool_temp=$($set_cool_temp_call)
 	therm_mode=$($therm_mode_call)
 	fan_status=$($fan_status_call)
+
 	clear
 
 	if [ "$therm_mode" == "3" ]
@@ -23,22 +24,20 @@ while true; do
 		echo "Cooling Requested"
 		echo "Requested Temp:" $set_cool_temp
 		echo "Is Fan ON? " $fan_status
-		if [ "$fan_status" == "True" ]
+		temp_diff=$(( $house_temp - $set_cool_temp ))
+		echo "Temp Difference:" $temp_diff
+		if (( $temp_diff < 2 && $temp_diff > 0 ))
 		then
-			temp_diff=$(( $house_temp - $set_cool_temp ))
-			echo "Temp Difference:" $temp_diff
-			if (( $temp_diff < 2  ))
-			then
-				echo "Low Cool"
-				curl -X GET 192.168.3.234/swamp/low-cool > /dev/null 2>&1
-			elif (( $temp_diff > 1  ))
-			then
-				echo "High Cool"
-                                curl -X GET 192.168.3.234/swamp/high-cool > /dev/null 2>&1
-			fi
-		else
-			echo "Swamp Cooler Off"
-			curl -X GET 192.168.3.234/swamp/off > /dev/null 2>&1
+			echo "Low Cool"
+			curl -X GET 192.168.3.234/swamp/low-cool > /dev/null 2>&1
+		elif (( $temp_diff > 1  ))
+		then
+			echo "High Cool"
+                        curl -X GET 192.168.3.234/swamp/high-cool > /dev/null 2>&1
+                elif (( $temp_diff == 0  ))
+        	then
+		        echo "Swamp Cooler Off"
+	                curl -X GET 192.168.3.234/swamp/off > /dev/null 2>&1
 		fi
 	else
 		echo "Swamp Cooler Off"
