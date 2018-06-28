edf875aa1f3c3d9b060df11ba10c1f52e70732ec
ezilagel <bzaff2012@gmail.com>
2018-06-28 06:31:18 +0000

    cleaned up output and set 60 sleep

--- temp_control.sh
+++ temp_control.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-app_path='/home/pi/apps/git/swamp-api'
+#app_path='/home/pi/apps/git/swamp-api'
 house_temp_call='/usr/bin/python therm.py -t'
 set_cool_temp_call='/usr/bin/python therm.py -s'
 therm_mode_call='/usr/bin/python therm.py -m'
 fan_status_call='/usr/bin/python therm.py -f'
 
-cd $app_path
+#cd $app_path
+
+clear
 
 while true; do
 
@@ -14,33 +16,34 @@ while true; do
 	set_cool_temp=$($set_cool_temp_call)
 	therm_mode=$($therm_mode_call)
 	fan_status=$($fan_status_call)
+	clear
 
 	if [ "$therm_mode" == "3" ]
 	then
 		echo "Cooling Requested"
 		echo "Requested Temp:" $set_cool_temp
+		echo "Is Fan ON? " $fan_status
 		if [ "$fan_status" == "True" ]
 		then
-			echo "House Fan On"
 			temp_diff=$(( $house_temp - $set_cool_temp ))
 			echo "Temp Difference:" $temp_diff
 			if (( $temp_diff < 2  ))
 			then
 				echo "Low Cool"
-				curl -X GET 192.168.3.234/swamp/low-cool
+				curl -X GET 192.168.3.234/swamp/low-cool > /dev/null 2>&1
 			elif (( $temp_diff > 1  ))
 			then
 				echo "High Cool"
-                                curl -X GET 192.168.3.234/swamp/high-cool
+                                curl -X GET 192.168.3.234/swamp/high-cool > /dev/null 2>&1
 			fi
 		else
-			echo "Swamp Cooler not needed to cool"
-			curl -X GET 192.168.3.234/swamp/off
+			echo "Swamp Cooler Off"
+			curl -X GET 192.168.3.234/swamp/off > /dev/null 2>&1
 		fi
 	else
 		echo "Swamp Cooler Off"
-		curl -X GET 192.168.3.234/swamp/off
+		curl -X GET 192.168.3.234/swamp/off > /dev/null 2>&1
 	fi
 	echo "Current Temp:" $house_temp
-	sleep 30
+	sleep 60
 done
